66baf60133ff360c5b3eb0d1dfec13eb0147c971
Riddzx <ridha8566@gmail.com>
2023-08-14 19:40:46 +0700

    update 1.2

--- dist/script.js
+++ dist/script.js
@@ -2,12 +2,15 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
+  const toTop = document.getElementById("to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
+    toTop.classList.remove("hidden");
     toTop.classList.add("block");
   } else {
     header.classList.remove("navbar-fixed");
+    toTop.classList.remove("block");
     toTop.classList.add("hidden");
   }
 };
