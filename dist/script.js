dd93c371b50b4ed543fa5757ca8b769739e653df
Riddzx <ridha8566@gmail.com>
2023-10-08 18:25:33 +0700

    .

--- dist/script.js
+++ dist/script.js
@@ -2,16 +2,11 @@
 window.onscroll = function () {
   const header = document.querySelector("header");
   const fixedNav = header.offsetTop;
-  const toTop = document.getElementById("to-top");
 
   if (window.pageYOffset > fixedNav) {
     header.classList.add("navbar-fixed");
-    toTop.classList.remove("hidden");
-    toTop.classList.add("block");
   } else {
     header.classList.remove("navbar-fixed");
-    toTop.classList.remove("block");
-    toTop.classList.add("hidden");
   }
 };
 
